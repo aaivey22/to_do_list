9784b5e32e730369ca3cfe5c5efeb04fdd0e502c
aaivey22 <angelawings22@gmail.com>
2022-02-28 18:49:16 -0500

    In js, a deleteTask func was added to remove the selected array item from the array list allTasks. A variable taskItemHtml was added to the addButton anonymous click func to create the necessary html for each list item. This functioned better than manipulating the pre-built html container. Currently, tasks are dynamically created an added or removed to the task list in the browser as well as the backend array when user clicks their respective buttons next to the task items.

--- script.js
+++ script.js
@@ -4,33 +4,44 @@
 // score and percentScore will display the total number of task items "completed" based on button clicks (only 1 click allowed per task item) as well as the average of total task items vs completed task items for a "efficiency" percentage score.
 let score = 0;
 let percentScore = 0;
+
 let allTasks = [];
-/*
-document.querySelector('.check').addEventListener
-    ('click', function () {
-        const guess = Number(document.querySelector('.guess').value);
-        console.log(guess, typeof guess);
-    });
-*/
 let numberOfTasks = 0;
+let taskId = 1;
 
 document.querySelector('#addButton').addEventListener
     ('click', function () {
         let taskItem = String(document.querySelector('#enterTask').value);
         allTasks.push(taskItem);
 
-        const taskText = allTasks[numberOfTasks];
+        const className = "taskItem" + taskId;
+        //const taskText = allTasks;
         const taskItemHtml = `
-            <li class="taskItem">${taskText}
-            <button class="complete" type="button" onclick="alert('Good Job!')">Complete</button>
-            <button class="delete" type="button" onclick="alert('Task Removed')">Delete</button>
+            <li id="${className}">${taskItem}
+            <button class="complete" type="button">Complete</button>
+            <button class="delete" type="button" onclick = "deleteTask(${taskId}, '${taskItem}')">Delete</button>
             </li>
             `
 
-        //document.querySelector('.taskItem').textContent = allTasks[numberOfTasks];
         document.querySelector('.taskItem').insertAdjacentHTML("beforeend", taskItemHtml);
         numberOfTasks++;
 
+        taskId++;
+
         console.log(allTasks, typeof allTasks);
         console.log(taskItem);
+
     });
+
+// i references the taskId to the specific list element to remove it from the "front end", NOT from the actual array
+function deleteTask(i, a) {
+    document.getElementById("taskItem" + i).remove();
+
+    console.log(allTasks, 'delete function');
+    console.log(allTasks.indexOf(a), 'index of item selected to delete');
+
+    allTasks.splice(allTasks.indexOf(a), 1);
+    numberOfTasks--;
+    console.log(allTasks, 'items remaining in array');
+
+};
